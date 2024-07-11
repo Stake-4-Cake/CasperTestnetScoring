803fd09f52ea6a59e289d25f3181a45dadfdf7ad
Stake4Cake <s4c.s@outlook.com>
2024-07-11 17:56:18 +0300

    Fix SSL Certs

--- app/config/settings.py
+++ app/config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", 'foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", 1))
+DEBUG = bool(os.environ.get("DEBUG", 0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", '*').split(" ")
 
